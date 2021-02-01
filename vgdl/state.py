4172db31b3c22f5a1d222ec70e0ab6598a7cfbcc
Chace Hayhurst <chace_hayhurst@brown.edu>
2021-01-31 19:10:56 -0500

    a

--- vgdl/state.py
+++ vgdl/state.py
@@ -227,8 +227,10 @@ class UltrasonicObserver(StateObserver):
         for sprite in sprites:
             if(sprite.id.split('.')[0] != 'background' and sprite.id.split('.')[0] != 'avatar'):
 
-                t1 = self.collidesX(avatar, sprite, self.game) or self.collidesX(sprite, avatar, self.game)
-                t2 = self.collidesY(avatar, sprite, self.game) or self.collidesY(sprite, avatar, self.game)
+                t1 = self.collidesX(avatar, sprite, self.game) 
+                # or self.collidesX(sprite, avatar, self.game)
+                t2 = self.collidesY(avatar, sprite, self.game) 
+                # or self.collidesY(sprite, avatar, self.game)
 
                 if(t1):
                     if(sprite.rect.y>avatar.rect.y and abs(sprite.rect.y-avatar.rect.y)<closestbottom
